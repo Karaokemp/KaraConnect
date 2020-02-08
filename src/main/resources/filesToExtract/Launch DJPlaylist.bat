bd5b448b39f4c799fb0299ff1f433900cd7f97f5
rabino <rabino@rabino-msi>
2020-02-08 22:54:06 +0200

    change firewall port to 80

--- src/main/resources/filesToExtract/Launch DJPlaylist.bat
+++ src/main/resources/filesToExtract/Launch DJPlaylist.bat
@@ -1,3 +1,3 @@
 set VLC_PLUGIN_PATH="C:\Program Files\VideoLAN\VLC\plugins"
-FirewallHelper.exe 4567
+FirewallHelper.exe 80
 java -Dfile.encoding=utf-8 -jar kara-vlc-1.0.0-SNAPSHOT-jar-with-dependencies.jar
